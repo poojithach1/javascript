3f06c5585318ed78ee2e75e8d83278235f3619ad
poojithach1 <chitturipoojitha1@gmail.com>
2021-12-25 15:33:44 +0530

    exp

---
+++ javascript30_course_excercises/localStorage/localStorage.js
@@ -0,0 +1,10 @@
+const addItems = document.querySelectorAll('.add-items');
+const itemsList = document.querySelectorAll('.plates');
+const items = [];
+
+function addItem(e) {
+  console.log('helo');
+}
+
+
+addItems.addEventListener('submit',addItem);
